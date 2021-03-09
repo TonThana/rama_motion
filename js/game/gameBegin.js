a4d0322a6072f2a067db531ded0ab89ea061a4ae
ton thana <chaeyklinthesthana@gmail.com>
2021-03-09 18:00:01 +0700

    dist

--- js/game/gameBegin.js
+++ js/game/gameBegin.js
@@ -6,7 +6,9 @@ import { MovingCircle } from './movingCircle'
 // some variables
 let SCREEN_WIDTH, SCREEN_HEIGHT
 let circles = []
-
+export const SMALL = "small"
+export const MEDIUM = "MEDIUM"
+export const LARGE = "LARGE"
 // show rules modal
 
 function showRuleModal() {
@@ -19,45 +21,100 @@ function showRuleModal() {
 
 
 export const init = () => {
-    showRuleModal()
+    // showRuleModal()
     const screenSize = getScreenSize()
     SCREEN_WIDTH = screenSize.width
     SCREEN_HEIGHT = screenSize.height
+
+    // // number of columns = 10
+    // const oneBoxWidth = SCREEN_WIDTH / 10
+    // console.log(oneBoxWidth)
+    // const numberOfRows = Math.floor(SCREEN_HEIGHT / oneBoxWidth)
+    // console.log(numberOfRows, " rows", "10 columns")
+
+
+    // position circles
+    let longerDim;
+    let shorterDim;
+    if (SCREEN_HEIGHT < SCREEN_WIDTH) {
+        longerDim = SCREEN_WIDTH
+        shorterDim = SCREEN_HEIGHT
+    } else {
+        longerDim = SCREEN_HEIGHT
+        shorterDim = SCREEN_WIDTH
+    }
+
+    // must 6 shorter dim (row)
+    const NUMBER_OF_ROWS = 8
+    const NUMBER_OF_COLS = 16
+    const oneBoxOnShorterDim = shorterDim / NUMBER_OF_ROWS
+    const oneBoxOnLongerDim = longerDim / NUMBER_OF_COLS
+    // let width be smaller value of the two
+    const oneBoxWidth = oneBoxOnShorterDim < oneBoxOnLongerDim ? oneBoxOnShorterDim : oneBoxOnLongerDim
+
+
+    // const NUMBER_OF_SMALL_CIRCLES = NUMBER_OF_COLS * NUMBER_OF_ROWS
+    // const NUMBER_OF_MEDIUM_CIRCLES = (NUMBER_OF_COLS / 2) * (NUMBER_OF_ROWS / 2)
+    // const NUMBER_OF_BIG_CIRCLES = 4
+
+    // const TOTAL_CIRCLES = NUMBER_OF_BIG_CIRCLES + NUMBER_OF_MEDIUM_CIRCLES + NUMBER_OF_SMALL_CIRCLES
+
+
+    const parentSvg = document.getElementById("circle-parent")
+    parentSvg.style.width = oneBoxWidth * NUMBER_OF_COLS
+    parentSvg.style.height = oneBoxWidth * NUMBER_OF_ROWS
+
+
+    // // small
+    for (let col = 0; col < NUMBER_OF_COLS; col += 1) {
+        for (let row = 0; row < NUMBER_OF_ROWS; row += 1) {
+            circles.push(new MovingCircle(parentSvg, col, row, oneBoxWidth, SMALL))
+        }
+    }
+
+    // // medium
+    for (let col = 0; col < NUMBER_OF_COLS / 2; col += 1) {
+        for (let row = 0; row < NUMBER_OF_ROWS / 2; row += 1) {
+            circles.push(new MovingCircle(parentSvg, col, row, oneBoxWidth * 2, MEDIUM))
+        }
+    }
+
+    // large 1 for each quadrant - special case needed 
+    for (let col = 0; col < NUMBER_OF_COLS / 2; col += 1) {
+        for (let row = 0; row < NUMBER_OF_ROWS / 2; row += 1) {
+            circles.push(new MovingCircle(parentSvg, col, row, oneBoxWidth * 4, LARGE))
+        }
+    }
+
+    // for (let row = 0; row < )
+
+    // console.log(
+    //     'wait 3 seconds'
+    // )
+    // setTimeout(() => {
+    //     startMotion()
+    //     // window.addEventListener('keydown', onKeyPress)
+    // }, 3000)
+
+    // position center of animation plane
     const centralCircle = document.getElementById("central-fix")
     centralCircle.style.fill = 'black'
-    centralCircle.style.cx = SCREEN_WIDTH / 2
-    centralCircle.style.cy = SCREEN_HEIGHT / 2
+    centralCircle.style.cx = oneBoxWidth * NUMBER_OF_COLS / 2
+    centralCircle.style.cy = oneBoxWidth * NUMBER_OF_ROWS / 2
 
     anime({
         targets: "#central-fix",
         opacity: [0, 1],
-        r: [0, SCREEN_WIDTH / 200],
+        r: [0, 5],
         duration: 500,
         easing: "linear"
     })
-    const NUMBER_OF_CIRCLES = 200
-    // generate moving circle
-    const parentSvg = document.getElementById("circle-parent")
-
-    for (let i = 0; i < NUMBER_OF_CIRCLES; i += 1) {
-        circles.push(new MovingCircle(parentSvg, i, SCREEN_WIDTH, SCREEN_HEIGHT))
-    }
-
-
-    console.log(
-        'wait 3 seconds'
-    )
-    setTimeout(() => {
-        startMotion()
-        // window.addEventListener('keydown', onKeyPress)
-    }, 3000)
 
 }
 
 export const exit = () => {
     circles.forEach(el => el.destroy())
     circles = []
-    // window.removeEventListener('keydown', onKeyPress)
 }
 
 
@@ -67,6 +124,7 @@ const getScreenSize = () => {
         width: window.innerWidth || document.body.clientWidth,
         height: window.innerHeight || document.body.clientHeight
     }
+    console.log(screensize)
     // not allowed if under certain dim
     return screensize
 }
--- js/game/movingCircle.js
+++ js/game/movingCircle.js
@@ -3,47 +3,51 @@ import anime from 'animejs'
 import getRandomInt from '../utils/getRandomInt'
 import getRandomFloat from '../utils/getRandomFloat'
 import getRandomItem from '../utils/getRandomItem'
-
+import { SMALL, MEDIUM, LARGE } from './gameBegin'
 
 export class MovingCircle {
     // represent 1 moving circle
-    constructor(parentSvg, index, SCREEN_WIDTH, SCREEN_HEIGHT) {
-        this.screen_width = SCREEN_WIDTH
-        this.screen_height = SCREEN_HEIGHT
-        this.circleIndex = index
+    constructor(parentSvg, col, row, box_width, type) {
+        this.type = type
+        this.box_width = box_width
+        this.circleIndex = `${row}-${col}-${type}`
+        this.col = col
+        this.row = row
         this.parentSvg = parentSvg
         this.circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
         this.config = this.generateCircleConfig()
+
         this.circle.setAttributeNS(null, 'cx', this.config.cx);
         this.circle.setAttributeNS(null, 'cy', this.config.cy);
         this.circle.setAttributeNS(null, 'r', this.config.r);
         this.circle.setAttributeNS(null, "class", "moving")
         this.circle.setAttributeNS(null, "id", this.circleIndex)
-        this.circle.setAttributeNS(null, 'style', `fill: none; stroke: blue; stroke-width: 1px`);
+        this.circle.setAttributeNS(null, 'style', `fill: none; stroke: #000; stroke-width: 1px`);
         this.circle.style.transformOrigin = `${this.config.xOrigin}px ${this.config.yOrigin}px`
         parentSvg.appendChild(this.circle)
+
+        // show all temp 
+        this.show()
+        this.animate()
     }
 
-    generateCircleConfig = () => {
-        // randomisation is current independent from one another
+    generateCircleConfig() {
+        // fix position based on row col box width
+        let r = this.type == SMALL ? 4 : this.type === MEDIUM ? 10 : 25
+        if (this.type === LARGE) {
 
-        let r = getRandomInt(2, 10)
-        let rotationRadius = getRandomInt(2, 100)
-        let swingTotalLength = (2 * r) + rotationRadius
+        }
+        let rotationRadius = this.box_width / 2 - (2 * r)
+        let xOrigin = this.col * this.box_width + this.box_width / 2
+        let yOrigin = this.row * this.box_width + this.box_width / 2
 
-        let minCenterX = swingTotalLength
-        let maxCenterX = this.screen_width - swingTotalLength
-        let minCenterY = swingTotalLength
-        let maxCenterY = this.screen_height - swingTotalLength
-
-        // x and y Center of Rotation
-        let xOrigin = getRandomFloat(minCenterX, maxCenterX)
-        let yOrigin = getRandomFloat(minCenterY, maxCenterY)
         let iAngle = 2 * Math.PI * Math.random()
         let cx = xOrigin + rotationRadius * Math.cos(iAngle)
         let cy = yOrigin + rotationRadius * Math.sin(iAngle)
         return { cx, cy, r, xOrigin, yOrigin }
     }
+
+
 
     getConfig() {
         return this.config
@@ -72,19 +76,20 @@ export class MovingCircle {
         return new Promise(resolve => {
             this.animId = anime({
                 targets: this.circle,
-                duration: () => getRandomInt(200, 6000),
+                duration: () => 3000,
                 easing: "linear",
                 rotate: [0, 720 * direction],
                 autoplay: false,
-                loop: false,
+                loop: true,
+                // loop: false,
                 begin: () => {
                     console.log("BEGIN")
-                    this.show()
+                    // this.show()
                 },
                 // currently trigger twice
                 complete: () => {
                     console.log("COMPLETE")
-                    this.stopAnimate()
+                    // this.stopAnimate()
                     resolve()
                 },
 
