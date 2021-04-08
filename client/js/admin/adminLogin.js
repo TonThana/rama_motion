b591131754c50908288a0e2a90d087ac237ea4c7
ton thana <chaeyklinthesthana@gmail.com>
2021-04-08 12:44:30 +0700

    fix function recreate

--- client/js/admin/adminLogin.js
+++ client/js/admin/adminLogin.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Control from '../control'
 
-export function adminLogin() {
+export const adminLogin = () => {
 
     let adminForm = document.getElementById('login-form')
     adminForm.removeEventListener('submit', login)
@@ -9,34 +9,36 @@ export function adminLogin() {
 
 
 
-    function login(ev) {
-        ev.preventDefault()
-        const password = ev.target.adminPassword.value
-        let errorMsgDiv = document.getElementById("loginErrorMsg")
 
-        axios.post("/api/adminAuthorise", { password }).then(res => {
-            if (res.data.ok) {
-                errorMsgDiv.innerText = ""
-                errorMsgDiv.style.color = ''
-                errorMsgDiv.classList.add("hidden")
-                // go on
-                if (res.data.hasOwnProperty("data")) {
-                    Control.show('admin', [res.data.data])
-                } else {
-                    Control.show('admin', ["no data found"])
-                }
+}
 
 
+function login(ev) {
+    ev.preventDefault()
+    const password = ev.target.adminPassword.value
+    let errorMsgDiv = document.getElementById("loginErrorMsg")
+    console.log("call login")
+    axios.post("/api/adminAuthorise", { password }).then(res => {
+        if (res.data.ok) {
+            errorMsgDiv.innerText = ""
+            errorMsgDiv.style.color = ''
+            errorMsgDiv.classList.add("hidden")
+            // go on
+            if (res.data.hasOwnProperty("data")) {
+                Control.show('admin', [res.data.data])
             } else {
-
-                errorMsgDiv.innerText = "wrong password"
-                errorMsgDiv.style.color = '#e3242b'
-                errorMsgDiv.classList.remove("hidden")
+                Control.show('admin', ["no data found"])
             }
-        }).catch(err => {
-            // console.log(err)
-            errorMsgDiv.innerText = "server error"
-        })
-    }
 
+
+        } else {
+
+            errorMsgDiv.innerText = "wrong password"
+            errorMsgDiv.style.color = '#e3242b'
+            errorMsgDiv.classList.remove("hidden")
+        }
+    }).catch(err => {
+        // console.log(err)
+        errorMsgDiv.innerText = "server error"
+    })
 }
