79fe8e1d4613ba48c276b18cb72de1a2f16ab2d1
ton thana <chaeyklinthesthana@gmail.com>
2021-04-22 15:15:27 +0700

    outline

--- client/js/game/movingCircle.js
+++ client/js/game/movingCircle.js
@@ -57,11 +57,11 @@ export class MovingCircle {
     setColor = () => {
         // console.log(this.colormode)
         if (this.colormode === "kw") {
-            this.circle.setAttributeNS(null, 'style', `fill: none; stroke: #000; stroke-width: 2px`);
+            this.circle.setAttributeNS(null, 'style', `fill: black; stroke: #000; stroke-width: 2px`);
             this.colorComb = ["none", "none"]
         } else {
             // set later - need animate
-            this.circle.setAttributeNS(null, 'style', `stroke: none; stroke-width: 0px`);
+            this.circle.setAttributeNS(null, 'style', ` stroke-width: 2px`);
             this.colormode === "rg" ? this.colorComb = ["#FF0000", "#00FF00"] : this.colorComb = ["#FFFF00", "#0000FF"]
         }
         // console.log(this.colorComb)
@@ -120,13 +120,30 @@ export class MovingCircle {
         // console.log(this.colorComb)
         window.addEventListener("keydown", this.onKeyDown)
         return new Promise(resolve => {
+            let color = shuffle([this.colorComb[0], this.colorComb[1]])
+
+            let stroke = ["none", "none"]
+            if (color[0] !== "none") {
+                stroke[0] = this.pSBC(-0.5, color[0])
+                stroke[1] = this.pSBC(-0.5, color[1])
+            }
+            // console.log(color, stroke)
+            this.circle.setAttributeNS(null, 'fill', color[0])
+            this.circle.setAttributeNS(null, 'stroke', stroke[0])
             this.animId = anime({
                 targets: this.circle,
                 duration: () => ANIM_DURATION,
                 easing: "linear",
                 rotate: [0, 720 * direction],
                 autoplay: false,
-                fill: shuffle([this.colorComb[0], this.colorComb[1]]),
+                fill: {
+                    value: color[1],
+                    easing: "easeInOutExpo"
+                },
+                stroke: {
+                    value: stroke[1],
+                    easing: "easeInOutExpo"
+                },
                 loop: false,
                 begin: () => {
                     // console.log("BEGIN")
@@ -140,6 +157,7 @@ export class MovingCircle {
                 },
 
             })
+
 
             this.animId.play()
         })
@@ -183,5 +201,33 @@ export class MovingCircle {
         })
     }
 
+
+    pSBC = (p, c0, c1, l) => {
+        let r, g, b, P, f, t, h, i = parseInt, m = Math.round, a = typeof (c1) == "string";
+        if (typeof (p) != "number" || p < -1 || p > 1 || typeof (c0) != "string" || (c0[0] != 'r' && c0[0] != '#') || (c1 && !a)) return null;
+        if (!this.pSBCr) this.pSBCr = (d) => {
+            let n = d.length, x = {};
+            if (n > 9) {
+                [r, g, b, a] = d = d.split(","), n = d.length;
+                if (n < 3 || n > 4) return null;
+                x.r = i(r[3] == "a" ? r.slice(5) : r.slice(4)), x.g = i(g), x.b = i(b), x.a = a ? parseFloat(a) : -1
+            } else {
+                if (n == 8 || n == 6 || n < 4) return null;
+                if (n < 6) d = "#" + d[1] + d[1] + d[2] + d[2] + d[3] + d[3] + (n > 4 ? d[4] + d[4] : "");
+                d = i(d.slice(1), 16);
+                if (n == 9 || n == 5) x.r = d >> 24 & 255, x.g = d >> 16 & 255, x.b = d >> 8 & 255, x.a = m((d & 255) / 0.255) / 1000;
+                else x.r = d >> 16, x.g = d >> 8 & 255, x.b = d & 255, x.a = -1
+            } return x
+        };
+        h = c0.length > 9, h = a ? c1.length > 9 ? true : c1 == "c" ? !h : false : h, f = this.pSBCr(c0), P = p < 0, t = c1 && c1 != "c" ? this.pSBCr(c1) : P ? { r: 0, g: 0, b: 0, a: -1 } : { r: 255, g: 255, b: 255, a: -1 }, p = P ? p * -1 : p, P = 1 - p;
+        if (!f || !t) return null;
+        if (l) r = m(P * f.r + p * t.r), g = m(P * f.g + p * t.g), b = m(P * f.b + p * t.b);
+        else r = m((P * f.r ** 2 + p * t.r ** 2) ** 0.5), g = m((P * f.g ** 2 + p * t.g ** 2) ** 0.5), b = m((P * f.b ** 2 + p * t.b ** 2) ** 0.5);
+        a = f.a, t = t.a, f = a >= 0 || t >= 0, a = f ? a < 0 ? t : t < 0 ? a : a * P + t * p : 0;
+        if (h) return "rgb" + (f ? "a(" : "(") + r + "," + g + "," + b + (f ? "," + m(a * 1000) / 1000 : "") + ")";
+        else return "#" + (4294967296 + r * 16777216 + g * 65536 + b * 256 + (f ? m(a * 255) : 0)).toString(16).slice(1, f ? undefined : -2)
+    }
+
 }
+
 
